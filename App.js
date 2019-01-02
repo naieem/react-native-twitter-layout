02223890b22b2b966f5c7ea0a72a224e964d0de5
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-01-02 16:52:24 +0600

    background forground with login in firebase done

--- App.js
+++ App.js
@@ -1,35 +1,117 @@
-import React, { Component } from 'react';
-import { View, Text,Button, StyleSheet, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView } from 'react-native';
-// import { Button } from 'react-native-elements';
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  ScrollView,
+  AppState
+} from 'react-native';
+import {Auth} from './db/db.config';
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      email: '',
+      password: '',
+      userInfo: {}
     };
+    this.loginUser = this.loginUser.bind(this);
+    this._handleAppStateChange = this._handleAppStateChange.bind(this);
+  }
+  componentDidMount(){
+    AppState.addEventListener('change', this._handleAppStateChange);
+    Auth.onAuthStateChanged(function(user) {
+      if (user) {
+        console.log(user);
+      }
+    });
+  }
+  _handleAppStateChange = (nextAppState) => {
+    if (nextAppState === 'active') {
+      console.log('App has come to the foreground!')
+    }else{
+      console.log('App has gone to the background!');
+      Auth.signOut().then((res)=>{
+        console.log(res);
+        console.log('signout done');
+      }).catch((error)=>{
+        console.log(error);
+      });
+    }
   }
 
+  loginUser = ()=> {
+    console.log('login called');
+    console.log(this.state);
+    Auth
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((result) => {
+        console.log('Login successfully');
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode === 'auth/wrong-password') {
+          alert('Wrong password.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+      });
+  }
   render() {
     return (
 
       <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
         <View style={styles.logoContainer}>
-          <Image source={require('./assets/logo.png')} style={{ height: 200, }}></Image>
+          <Image
+            source={require('./assets/logo.png')}
+            style={{
+            height: 200
+          }}></Image>
         </View>
         <ScrollView style={styles.loginFormContainer}>
 
-          <View style={{ marginBottom: 20, alignItems: 'center' }}>
-            <Text style={{ fontWeight: 'bold', fontSize: 26 }}>Login</Text>
+          <View
+            style={{
+            marginBottom: 20,
+            alignItems: 'center'
+          }}>
+            <Text
+              style={{
+              fontWeight: 'bold',
+              fontSize: 26
+            }}>Login</Text>
           </View>
           <View>
             <TextInput
               style={styles.input}
               placeholder="Username"
-            />
+              value={this.state.email}
+              onChangeText={(text) => {
+                this.setState({email: text});
+                console.log(this.state.email);
+              }}/>
             <TextInput
               style={styles.input}
               placeholder="Password"
-            />
-            <TouchableOpacity style={{justifyContent:'center',alignItems:'center'}}>
+              value={this.state.password}
+              secureTextEntry={true}
+              onChangeText={(text) => {
+                this.setState({password: text});
+                console.log(this.state.password);
+              }}/>
+            <TouchableOpacity
+              style={{
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+              onPress={this.loginUser}>
               <Text style={styles.submitStyle}>Login</Text>
             </TouchableOpacity>
           </View>
@@ -43,16 +125,16 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   logoContainer: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   loginFormContainer: {
     flex: 1,
-    paddingHorizontal: 30,
+    paddingHorizontal: 30
   },
   input: {
     borderRadius: 10,
@@ -63,12 +145,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10
   },
   submitStyle: {
-    width:200,
-    textAlign:'center',
+    width: 200,
+    textAlign: 'center',
     paddingVertical: 10,
     backgroundColor: '#0AF5C3',
-    borderRadius:10,
-    color:'#fff',
-    fontWeight:'bold'
+    borderRadius: 10,
+    color: '#fff',
+    fontWeight: 'bold'
   }
 });
---
+++ db/db.config.js
@@ -0,0 +1,22 @@
+import React, {
+    Component
+} from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDjtVishNAmMqCl2HdfghOUkebRwaqjBME",
+    authDomain: "ecommerce-8726b.firebaseapp.com",
+    databaseURL: "https://ecommerce-8726b.firebaseio.com",
+    projectId: "ecommerce-8726b",
+    storageBucket: "ecommerce-8726b.appspot.com",
+    messagingSenderId: "387167767867"
+};
+firebase.initializeApp(config);
+const Auth = firebase.auth();
+const DB = firebase.database();
+
+export { Auth, DB };
